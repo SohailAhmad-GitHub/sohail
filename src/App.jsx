66b92900fd6c3c9d1c0544746eb63d675f3bfeb2
SohailAhmad-GitHub <105258157+SohailAhmad-GitHub@users.noreply.github.com>
2023-05-31 10:56:25 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+   <div className="container">
+    <div className="box">box 1</div>
+    <div className="box">box 2</div>
+    <div className="box">box 3</div>
+    <div className="box">box 4</div>
+   </div>
+    </>
+  )
+}
+
+export default App
